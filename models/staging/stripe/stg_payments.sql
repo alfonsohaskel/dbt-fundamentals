d1054cbb0fa4c09c3aef82981d0fcdb918ff1082
alfonso7797@gmail.com <alfonso7797@gmail.com>
2024-03-04 18:05:13 +0000

    added tests

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -8,8 +8,8 @@ with payment as (
         amount / 100 as amount,
         created as created_at
 
-    from raw.stripe.payment
-
+    from {{ source('stripe', 'payment')}}
+    
 )
 
 select * from payment
